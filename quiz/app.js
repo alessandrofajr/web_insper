8b2e5c176c0f0884766dc3168f867abc0c88781e
Alessandro Feitosa Jr <alessandrofajunior@gmail.com>
2022-05-06 16:53:00 -0300

    Add comments

--- quiz/app.js
+++ quiz/app.js
@@ -1,37 +1,35 @@
-let billionaires = document.querySelectorAll('img')
-let question = document.querySelector( '.question' )
-let quiz = document.querySelector( '.quiz' )
-let alternatives = document.querySelectorAll( 'li' )
-let result
-let answer
-let options
-let selectedBillionaire
-let inactiveBillionaires = document.querySelector( '.billionaires' )
-let musk = document.querySelector( '.Musk' )
-let bezos = document.querySelector( '.Bezos' )
-let arnault = document.querySelector( '.Arnault' )
-let playAgain = document.querySelector( '.play-again' )
+let billionaires = document.querySelectorAll('img') //variável para selecionar as imagens dos bilionários
+let question = document.querySelector( '.question' ) //variável para selecionar o bloco de texto que apresenta dados e pergunta
+let alternatives = document.querySelectorAll( 'li' ) //variável para selecionar as opções de resposta
+let result //declara variável que será usada para dizer se usuário acertou ou errou
+let answer //declara variável que mostrará a resposta certa
+let options //declara variável que permitirá mudar quais opções aparecem no quiz
+let selectedBillionaire //declara variável que guarda qual bilionário foi escolhido
+let inactiveBillionaires = document.querySelector( '.billionaires' ) //variável que guarda qual bilionário foi escolhido e impede mudança
+let musk = document.querySelector( '.Musk' ) //variável que seleciona a classe da img do bilionário escolhido para colocar borda ao redor da foto após clique
+let bezos = document.querySelector( '.Bezos' ) //variável que seleciona a classe da img do bilionário escolhido para colocar borda ao redor da foto após clique
+let arnault = document.querySelector( '.Arnault' ) //variável que seleciona a classe da img do bilionário escolhido para colocar borda ao redor da foto após clique
+let playAgain = document.querySelector( '.play-again' ) //variável que seleciona a classe que mostra o botão de jogar novamente
+
+
+// função que checa bilionário escolhido após o clique
 
 function checkBillionaire(event) {
 
     let billionaires = event.target
-    selectedBillionaire = billionaires.className
-    answer = document.querySelector( '.answer-' + selectedBillionaire)
-    options = document.querySelector( '.options-' + selectedBillionaire)
+    selectedBillionaire = billionaires.className //guarda o nome da classe do bilionário escolhido
+    answer = document.querySelector( '.answer-' + selectedBillionaire) //seleciona a classe .answer do bilionário correspondete
+    options = document.querySelector( '.options-' + selectedBillionaire) 
 
     if (billionaires.classList.contains('Musk')) {
-        question.style.display = 'initial'
-        options.style.display = 'initial'
-        inactiveBillionaires.classList.add('inativo')
-        musk.classList.add( 'img-clicked' )
-        document.getElementById('person').innerHTML = "Elon Musk";
+        question.style.display = 'initial' //exibe a pergunta do Musk. Há mudanças para cada bilionário
+        options.style.display = 'initial' //exibe as opções para o Musk. Há mudanças para cada bilionário
+        inactiveBillionaires.classList.add('inativo') //desativa a opção de clicar em outro bilionário
+        musk.classList.add( 'img-clicked' ) //coloca a borda no bilionário selecionado
+        document.getElementById('person').innerHTML = "Elon Musk"; //altera o corpo do texto que descreve a fortuna
         document.getElementById('money').innerHTML = "US$ 219 bilhões";
         document.getElementById('company').innerHTML = "Tesla";
         document.getElementById('person1').innerHTML = "Elon Musk";
-        // document.getElementById('first-alternative').innerHTML = "180 anos";
-        // document.getElementById('second-alternative').innerHTML = "2500 anos";
-        // document.getElementById('third-alternative').innerHTML = "6900 anos";
-        // document.getElementById('fourth-alternative').innerHTML = "7400 anos";
     }   else if (billionaires.classList.contains('Bezos')) {
         question.style.display = 'initial' 
         options.style.display = 'initial'
@@ -41,10 +39,6 @@ function checkBillionaire(event) {
         document.getElementById('money').innerHTML = "US$ 171 bilhões";
         document.getElementById('company').innerHTML = "Amazon";
         document.getElementById('person1').innerHTML = "Jeff Bezos";
-        // document.getElementById('first-alternative').innerHTML = "160 anos";
-        // document.getElementById('second-alternative').innerHTML = "3200 anos";
-        // document.getElementById('third-alternative').innerHTML = "5400 anos";
-        // document.getElementById('fourth-alternative').innerHTML = "6300 anos";
     }   else if(billionaires.classList.contains('Arnault')) {
         question.style.display = 'initial' 
         options.style.display = 'initial'
@@ -54,28 +48,24 @@ function checkBillionaire(event) {
         document.getElementById('money').innerHTML = "US$ 158 bilhões";
         document.getElementById('company').innerHTML = "LVMH (Louis Vuitton)";
         document.getElementById('person1').innerHTML = "Bernard Arnault";
-        // document.getElementById('first-alternative').innerHTML = "130 anos";
-        // document.getElementById('second-alternative').innerHTML = "3100 anos";
-        // document.getElementById('third-alternative').innerHTML = "5000 anos";
-        // document.getElementById('fourth-alternative').innerHTML = "6100 anos";
 }
 }
 
 for (let billionaire of billionaires){
     billionaire.addEventListener('click', checkBillionaire)
-}
+} // procura por cliques nas fotos dos bilionários
 
 function checkAnswer(event) {
     let alternative = event.target
-    alternative.classList.add( 'clicked' )
-    options.classList.add( 'quiz-clicked' )
-    playAgain.style.display = 'flex'
+    alternative.classList.add( 'clicked' ) // adiciona classe que altera cor das opções após clique
+    options.classList.add( 'quiz-clicked' ) //impede novos cliques nas opções do quiz
+    playAgain.style.display = 'flex' // muda o display para exibir o botão de jogar de novo
     
-    result = document.querySelector( '.result-' + selectedBillionaire)
+    result = document.querySelector( '.result-' + selectedBillionaire) // seleciona a classe do bilionário correspondente
 
     if ( alternative.classList.contains( 'correct' ) ) {
-        answer.style.display = 'initial' 
-        result.textContent = 'Acertou!'
+        answer.style.display = 'initial' // mostra o infográfico do bilionário correspondente
+        result.textContent = 'Acertou!' // mostra se a pessoa acertou
     
       } else {
         answer.style.display = 'initial' 
@@ -84,8 +74,10 @@ function checkAnswer(event) {
 }
 for (let alternative of alternatives){
     alternative.addEventListener('click', checkAnswer)
-}
+} // procura por cliques nas alternativas do quiz
 
+
+// Adiciona função de recarregar a página em um botão
 let refreshButton = document.querySelector('.refresh-button');
 
 let refreshPage = () => {
