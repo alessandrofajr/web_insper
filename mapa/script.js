dab75c148cb91eca78fe0eb65c48bc8a2184de08
Alessandro Feitosa Jr <alessandrofajunior@gmail.com>
2022-07-02 13:58:04 -0300

    commenting code

--- mapa/script.js
+++ mapa/script.js
@@ -3,8 +3,6 @@ let mapaDados;
 
 // como a função carrega um arquivo, usamos o termo async
 // para indicar que ela vai esperar o carregamento
-
-//mudar minha base e colocar o código de área (ver no site do IGBE)
 async function loadMapData(){
     let mapaUrl='https://servicodados.ibge.gov.br/api/v3/malhas/paises/BR?formato=application/json&qualidade=intermediaria&intrarregiao=UF';
     let dadosUrl='dados_ibge_anatel.json';
@@ -33,18 +31,7 @@ async function loadMapData(){
     projecao.scale(900);
     projecao.translate([width/2, height/2]);
 
-    // let dadosDensidade = []
-    // // cria uma array só com as densidades, para
-    // // poder calcular o valor mínimo e máximo
-    // for (d of mapaDados) {
-    //     dadosDensidade.push(d.densidade);
-    // }
-
-    // console.log(d.densidade)
-
-    //  no domain você coloca o máximo e o mínimo da sua esscala
-    // por exemplo se a densidade é um valor de 1.5 a 31,
-    // Coloquei 0 como o mínimo e 32 como o máximo p/ arredondar
+    // define o range de cores e escala a ser usada pelo mapa
     var calculaCor = d3.scaleLinear()
     .domain([0, 32])
     .range(["#FFF5F0", "#A00014"])
@@ -56,16 +43,15 @@ async function loadMapData(){
         .attr("class", "uf")
         .attr("id", (d) => d.properties.codarea)
         .style("fill", function(d){
-            // primeiro, pegamoss o código da área de cada elemento
+            // pega o código da área de cada elemento
             let codigo = d.properties.codarea;
             let cor;
             
-            // filtramos os dados para pegar somente o item do json
+            // filtra os dados para pegar somente o item do json
             // que tem o código da área
             let dadoDoEstado = mapaDados.filter(function(item){
                 return item.id == codigo;
             }); 
-            // console.log(dadoDoEstado[0]);
             if(dadoDoEstado[0]){ 
                 // a função calculaCor na linha 46 precisa do valor usado 
                 // para pintar o mapa. No caso, densidade. ela retorna uma cor
@@ -93,16 +79,10 @@ function marcaUF(elemento, dados){
         return item.id == codigo;
     });
 
-    // let dadosUF = mapaDados.filter(function(item){
-    //     return item.id == codigo;
-    // }); filtrar meus dados aqui
-
     let nome = dadosUF[0].nome;
     let densidade = dadosUF[0].densidade
-    // let uf = dadosUF[0].sigla;
     d3.select(elemento).classed("ativo", true);    
     d3.select('#uf-titulo').node().textContent = nome + " registrou " + parseFloat(densidade).toFixed(1) + " acessos de internet banda larga a cada 100 habitantes";
-    // d3.select('#uf-valor').node().textContent = valorindice; //carregar o JSON
 }
 
 function desmarcaUF(elemento){
